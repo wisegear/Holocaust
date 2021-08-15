0191fbe73bfd092db26c2c1737144b821deba1c1
wisegear <lee@wisener.net>
2021-08-15 22:47:36 +0100

    fixes

--- app/View/Components/blogsidebar.php
+++ app/View/Components/blogsidebar.php
@@ -24,9 +24,9 @@ class blogsidebar extends Component
      */
     public function render()
     {
-        $categories = \App\Models\BlogCategories::with('BlogPosts')->get();
-        $featured = \App\Models\BlogPosts::with('Users')->where('featured', true)->orderBy('created_at', 'desc')->limit(3)->get();
-        $unpublished = \App\Models\BlogPosts::where('published', false)->get();
+        $categories = App\Models\BlogCategories::with('BlogPosts')->get();
+        $featured = App\Models\BlogPosts::with('Users')->where('featured', true)->orderBy('created_at', 'desc')->limit(3)->get();
+        $unpublished = App\Models\BlogPosts::where('published', false)->get();
         $popular_tags = DB::table('post_tags')
             ->leftjoin('blog_tags', 'blog_tags.id', '=', 'post_tags.tag_id')
             ->select('post_tags.tag_id', 'name', DB::raw('count(*) as total'))
