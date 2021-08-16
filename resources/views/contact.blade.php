99855d2c5526daa62afb5ae84eedf0ce4a741dc6
wisegear <lee@wisener.net>
2021-08-16 17:18:34 +0100

    fixes

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -5,6 +5,7 @@
         <div class="prose max-w-screen-xl py-4">
         <h2>Getting in touch</h2>
         <p>For now you can contact me by email using lee@wisener.net. I will put a contact form on this page soon.</p>
+        <p>Alternatively, if you are a logged in member on the site you can now use the support system availble in the user menu in the top right of the screen.</p>
         <p><b>Lee Wisener</b></p>
         </div>
     </div>
