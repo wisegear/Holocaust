41a68c4c5c9585fdb098ba62cc993fd45ff0d8a5
wisegear <lee@wisener.net>
2021-08-17 10:11:30 +0100

    fixes

--- resources/views/timeline/index.blade.php
+++ resources/views/timeline/index.blade.php
@@ -2,9 +2,10 @@
 
    <!-- Page header -->
 
-      <div class="mb-5">
+      <div class="mb-5 w-1/2 mx-auto">
          <h2 class="text-xl font-bold text-center">Timeline</h2>
-         <p class="text-center text-gray-500">Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore, perspiciatis.</p>
+         <p class="text-center text-gray-500 text-sm">Use the timeline to search for events related to the Holocaust.  Each page has 50 events starting from
+            the earliest event through to the last.  You can select a specific year or search for a specific event.  The results will always be in chronological order.</p>
       </div>
 
    <!-- Page Content -->
@@ -14,22 +15,23 @@
          </div>
       @endcan
 
-      <div class="my-10">
-         <ul class="flex justify-center gap-5">
-            <li class="border rounded-md p-1 text-sm bg-gray-100"><a href="#">1933</a></li>
-            <li class="border rounded-md p-1 text-sm bg-gray-100"><a href="#">1934</a></li>
-            <li class="border rounded-md p-1 text-sm bg-gray-100"><a href="#">1935</a></li>
-            <li class="border rounded-md p-1 text-sm bg-gray-100"><a href="#">1936</a></li>
-            <li class="border rounded-md p-1 text-sm bg-gray-100"><a href="#">1937</a></li>
-            <li class="border rounded-md p-1 text-sm bg-gray-100"><a href="#">1938</a></li>
-            <li class="border rounded-md p-1 text-sm bg-gray-100"><a href="#">1939</a></li>
-            <li class="border rounded-md p-1 text-sm bg-gray-100"><a href="#">1940</a></li>
-            <li class="border rounded-md p-1 text-sm bg-gray-100"><a href="#">1941</a></li>
-            <li class="border rounded-md p-1 text-sm bg-gray-100"><a href="#">1942</a></li>
-            <li class="border rounded-md p-1 text-sm bg-gray-100"><a href="#">1943</a></li>
-            <li class="border rounded-md p-1 text-sm bg-gray-100"><a href="#">1944</a></li>
-            <li class="border rounded-md p-1 text-sm bg-gray-100"><a href="#">1945</a></li> 
-            <li class="border rounded-md p-1 text-sm bg-gray-100"><a href="#">Beyond</a></li> 
+      <div class="my-10 w-4/5 mx-auto">
+         <ul class="flex justify-evenly gap-5">
+            <li class="border rounded-md p-1 text-sm hover:bg-red-200"><a href="#">Earlier</a></li>
+            <li class="border rounded-md p-1 text-sm hover:bg-red-200"><a href="#">1933</a></li>
+            <li class="border rounded-md p-1 text-sm hover:bg-red-200"><a href="#">1934</a></li>
+            <li class="border rounded-md p-1 text-sm hover:bg-red-200"><a href="#">1935</a></li>
+            <li class="border rounded-md p-1 text-sm hover:bg-red-200"><a href="#">1936</a></li>
+            <li class="border rounded-md p-1 text-sm hover:bg-red-200"><a href="#">1937</a></li>
+            <li class="border rounded-md p-1 text-sm hover:bg-red-200"><a href="#">1938</a></li>
+            <li class="border rounded-md p-1 text-sm hover:bg-red-200"><a href="#">1939</a></li>
+            <li class="border rounded-md p-1 text-sm hover:bg-red-200"><a href="#">1940</a></li>
+            <li class="border rounded-md p-1 text-sm hover:bg-red-200"><a href="#">1941</a></li>
+            <li class="border rounded-md p-1 text-sm hover:bg-red-200"><a href="#">1942</a></li>
+            <li class="border rounded-md p-1 text-sm hover:bg-red-200"><a href="#">1943</a></li>
+            <li class="border rounded-md p-1 text-sm hover:bg-red-200"><a href="#">1944</a></li>
+            <li class="border rounded-md p-1 text-sm hover:bg-red-200"><a href="#">1945</a></li> 
+            <li class="border rounded-md p-1 text-sm hover:bg-red-200"><a href="#">Later</a></li> 
          </ul>
       </div>
                 
@@ -37,8 +39,8 @@
          @foreach ($events->chunk(2) as $two_events )
             @foreach($two_events as $event)
                @if($loop->odd)
-                  <div class="flex space-x-10">
-                     <div class="w-1/2 flex items-center space-x-5 border rounded-md">
+                  <div class="flex space-x-10  justify-start">
+                     <div class="w-3/4 flex items-center space-x-5 border rounded-md">
                         <div class="border-r p-4 text-center text-red-500">
                            <span class="text-sm font-bold">{{ date('d M', strtotime($event->event_date)) }}</span>
                            <span class="font-bold">{{ date('Y', strtotime($event->event_date)) }}</span>
@@ -48,17 +50,11 @@
                            <p class="text-sm">{{ $event->description }}</p>
                         </div>
                      </div>
-                     <div class="w-1/2">
-                        <!-- Intentionally blank -->
-                     </div>
                   </div>
                @elseif($loop->even)
-                  <div class="flex space-x-10">
-                     <div class="w-1/2">
-                        <!-- Intentionally blank -->
-                     </div>
-                     <div class="w-1/2 flex items-center space-x-5 border rounded-md">
-                     <div class="p-4">
+                  <div class="flex space-x-10 justify-end">
+                     <div class="w-3/4 flex items-center space-x-5 border rounded-md">
+                     <div class="p-4 space-y-4">
                            <h2 class="font-bold">{{ $event->title }}</h2>
                            <p class="text-sm">{{ $event->description }}</p>
                         </div>
@@ -66,7 +62,6 @@
                            <span class="text-sm font-bold">{{ date('d M', strtotime($event->event_date)) }}</span>
                            <span class="font-bold">{{ date('Y', strtotime($event->event_date)) }}</span>
                         </div>
-
                      </div>
                   </div>     
                @endif
